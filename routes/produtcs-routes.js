43b6f68c88ff3a442c570841ebed1f59041f8456
XX989tmx <issacgravity8@gmail.com>
2020-11-05 08:35:44 +0900

    adds sample data

--- routes/produtcs-routes.js
+++ routes/produtcs-routes.js
@@ -2,14 +2,136 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/all", function (req, res) {
-  res.json({ res: "all product page" });
+  let arr = [
+    {
+      id: "p1",
+      name: "ヨシカワ ホットサンドメーカー",
+      price: {
+        price: 1473,
+        currency: "JPY",
+      },
+      deliveryDate: "11/20 2020",
+      brand: "ヨシカワ(Yoshikawa)",
+      parentCategory: ["鍋・フライパン"],
+      ancestorCategories: [
+        "鍋・フライパン",
+        "キッチン用品",
+        "ホーム＆キッチン",
+      ],
+      categories: ["直火式ホットサンド・ワッフルメーカー"],
+      stockQuantity: 16,
+      isStock: true,
+    },
+    {
+      id: "p2",
+      name: "おいしい水",
+      price: {
+        price: 150,
+        currency: "JPY",
+      },
+      deliveryDate: "11/20 2020",
+      brand: "伊藤園",
+      parentCategory: ["水、飲料"],
+      ancestorCategories: ["水、飲料", "食品、日用品"],
+      categories: ["ミネラルウォーター"],
+      stockQuantity: 10,
+      isStock: true,
+    },
+  ];
+
+  res.json({ arr });
 });
 
 router.get("/specificProduct/:product", function (req, res) {
-  res.json({ res: "specific page" });
+  //:product = productName or productId
+  const productName = req.params.product;
+  let arr = [
+    {
+      id: "p1",
+      name: "ヨシカワ ホットサンドメーカー",
+      price: {
+        price: 1473,
+        currency: "JPY",
+      },
+      deliveryDate: "11/20 2020",
+      brand: "ヨシカワ(Yoshikawa)",
+      parentCategory: ["鍋・フライパン"],
+      ancestorCategories: [
+        "鍋・フライパン",
+        "キッチン用品",
+        "ホーム＆キッチン",
+      ],
+      categories: ["直火式ホットサンド・ワッフルメーカー"],
+      stockQuantity: 16,
+      isStock: true,
+    },
+    {
+      id: "p2",
+      name: "おいしい水",
+      price: {
+        price: 150,
+        currency: "JPY",
+      },
+      deliveryDate: "11/20 2020",
+      brand: "伊藤園",
+      parentCategory: ["水、飲料"],
+      ancestorCategories: ["水、飲料", "食品、日用品"],
+      categories: ["ミネラルウォーター"],
+      stockQuantity: 10,
+      isStock: true,
+    },
+  ];
+
+  const result = arr.filter((v) => {
+    return v.name === productName;
+  });
+
+  res.json({ result });
 });
 router.get("/productIndex/:category", function (req, res) {
-  res.json({ res: " product index page" });
+  const category = req.params.category;
+  let arr = [
+    {
+      id: "p1",
+      name: "ヨシカワ ホットサンドメーカー",
+      price: {
+        price: 1473,
+        currency: "JPY",
+      },
+      deliveryDate: "11/20 2020",
+      brand: "ヨシカワ(Yoshikawa)",
+      parentCategory: ["鍋・フライパン"],
+      ancestorCategories: [
+        "鍋・フライパン",
+        "キッチン用品",
+        "ホーム＆キッチン",
+      ],
+      categories: ["直火式ホットサンド・ワッフルメーカー"],
+      stockQuantity: 16,
+      isStock: true,
+    },
+    {
+      id: "p2",
+      name: "おいしい水",
+      price: {
+        price: 150,
+        currency: "JPY",
+      },
+      deliveryDate: "11/20 2020",
+      brand: "伊藤園",
+      parentCategory: ["水、飲料"],
+      ancestorCategories: ["水、飲料", "食品、日用品"],
+      categories: ["ミネラルウォーター"],
+      stockQuantity: 10,
+      isStock: true,
+    },
+  ];
+
+  const result = arr.filter((v) => {
+    return v.categories.includes(category);
+  });
+
+  res.json({ result });
 });
 
 router.post("/createNewProduct", function (req, res) {
