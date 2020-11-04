24bc50db43b29635183030b81b5baf14c68aad72
XX989tmx <issacgravity8@gmail.com>
2020-11-05 08:13:02 +0900

    adds product schema

---
+++ models/product.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  price: {
+    price: { type: Number, required: true },
+    currency: { type: String, required: true },
+  },
+  deliveryDate: { type: String, required: true },
+  brand: { type: String, required: true },
+  parentCategory: [{ type: String }],
+  ancestorCategories: [{ type: String }],
+  categories: [{ type: String }],
+  stockQuantity: { type: Number },
+  isStock: { type: Boolean },
+});
+
+mongoose.model("Product", productSchema);
