b152a6208b1096c846e4ecb222caadab6fc3a373
XX989tmx <issacgravity8@gmail.com>
2020-11-04 19:52:44 +0900

    adds product routes

--- routes/produtcs-routes.js
+++ routes/produtcs-routes.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  res.json({ res: "this is product page" });
+router.get("/all", function (req, res) {
+  res.json({ res: "all product page" });
+});
+
+router.get("/specificProduct/:product", function (req, res) {
+  res.json({ res: "specific page" });
+});
+router.get("/productIndex/:category", function (req, res) {
+  res.json({ res: " product index page" });
+});
+
+router.post("/createNewProduct", function (req, res) {
+  res.json({ res: "add new product page" });
+});
+
+router.get("/newlyAddedRankingTop50/:category", function (req, res) {
+  res.json({ res: "newlyAddedRankingTop50 " });
+});
+
+router.get("/bestsellerRankingTop50/:category", function (req, res) {
+  res.json({ res: "bestsellerRankingTop50" });
+});
+
+router.get("/wishlistRankingTop50/:category", function (req, res) {
+  res.json({ res: "wishlistRankingTop50" });
+});
+
+router.get("/bargain/:category", function (req, res) {
+  res.json({ res: "bargain campaign products" });
 });
 
 module.exports = router;
