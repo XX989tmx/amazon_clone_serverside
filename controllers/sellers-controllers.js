07de474505bce7e1d5f52f4efad0a6eaeb0c838e
XX989tmx <issacgravity8@gmail.com>
2020-11-05 11:55:15 +0900

    adds sample to signup

--- controllers/sellers-controllers.js
+++ controllers/sellers-controllers.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const expressValidator = require('express-validator')
-const mongoose = require('mongoose')
-
+const express = require("express");
+const expressValidator = require("express-validator");
+const mongoose = require("mongoose");
 
 const signup = async (req, res, next) => {
-  res.json({ res: "seller signup page" });
+  const { name, email, password } = req.body;
+
+  const createdSeller = {
+    name: name,
+    email: email,
+    password: password,
+  };
+
+  res.json({ createdSeller });
 };
 
 const login = async (req, res, next) => {
