10c3812ca4c1c0eef94585d321a58b2da142d83f
XX989tmx <issacgravity8@gmail.com>
2020-11-09 19:01:27 +0900

    adds authentication to create wishlist route

--- routes/wishlists-routes.js
+++ routes/wishlists-routes.js
@@ -1,13 +1,12 @@
 const express = require("express");
+const { check } = require("express-validator");
 const expressValidator = require("express-validator");
 const wishlistsControllers = require("../controllers/wishlists-controllers");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
-router.post(
-  "/createNewWishlist/:userId",
-  wishlistsControllers.createNewWishlist
-);
+
 
 router.post(
   "/addProductToWishlist/:userId/:productId/:wishlistId",
@@ -27,6 +26,13 @@ router.delete(
 router.get(
   "/getSpecificWishlist/:userId/:wishlistId",
   wishlistsControllers.getSpecificWishlist
+);
+
+router.use(checkAuth);
+
+router.post(
+  "/createNewWishlist/:userId",
+  wishlistsControllers.createNewWishlist
 );
 
 module.exports = router;
