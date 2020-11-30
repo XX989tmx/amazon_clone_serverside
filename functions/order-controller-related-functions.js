30b17c4974ac13490097e71077be0a054529e0de
XX989tmx <issacgravity8@gmail.com>
2020-12-01 04:29:03 +0900

    adds some function 2

--- functions/order-controller-related-functions.js
+++ functions/order-controller-related-functions.js
@@ -1,8 +1,10 @@
+const mongoose = require("mongoose");
+const Order = require("../models/order");
 /**
  *
  *
  * @param {number} price
- * @return {number} 
+ * @return {number}
  */
 function calculateAcquirableAmazonPoint(price) {
   if (typeof price !== "number") {
@@ -11,6 +13,29 @@ function calculateAcquirableAmazonPoint(price) {
   }
   const acquirablePoint = Math.round(price * 0.01);
   return acquirablePoint;
+}
+
+async function calculateTotalAmountOfPriceOfOrderOfAllTime(userId) {
+  let orders;
+  try {
+    orders = await Order.find({ user: userId });
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+
+  if (!orders) {
+    const error = new Error("Error occurred. Failed to load data.");
+    return next(error);
+  }
+  
+  let sum = 0;
+  for (let i = 0; i < orders.length; i++) {
+      const order = orders[i];
+      sum += order.totalPrice;
+  }
+  
+  return sum;
 }
 
 exports.calculateAcquirableAmazonPoint = calculateAcquirableAmazonPoint;
