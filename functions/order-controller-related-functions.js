d465fd21ae6292a13bbd8be05689fdeb403e37d0
XX989tmx <issacgravity8@gmail.com>
2020-12-01 04:52:19 +0900

    adds some function 3

--- functions/order-controller-related-functions.js
+++ functions/order-controller-related-functions.js
@@ -28,18 +28,49 @@ async function calculateTotalAmountOfPriceOfOrderOfAllTime(userId) {
     const error = new Error("Error occurred. Failed to load data.");
     return next(error);
   }
-  
+
   let sum = 0;
   for (let i = 0; i < orders.length; i++) {
-      const order = orders[i];
-      sum += order.totalPrice;
+    const order = orders[i];
+    sum += order.totalPrice;
   }
 
-//   for (const { totalPrice } of orders) {
-//     sum += totalPrice;
-//   }
-  
+  //   for (const { totalPrice } of orders) {
+  //     sum += totalPrice;
+  //   }
+
   return sum;
+}
+
+async function calculateTotalAmountOfPriceOfOrderOfThisMonth(userId) {
+  let orders;
+  try {
+    orders = await Order.find({ user: userId });
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+
+  if (!orders) {
+    const error = new Error("Error occurred. Failed to load data.");
+    return next(error);
+  }
+  let thisMonth = new Date().getMonth();
+  let ordersOfThisMonth = [];
+
+  for (const order of orders) {
+    const orderedMonth = order.dateOrdered.getMonth();
+    if (orderedMonth === thisMonth) {
+      ordersOfThisMonth.push(order);
+    }
+  }
+
+  if (ordersOfThisMonth.length === 0) {
+    const error = new Error("今月のご注文履歴はありません。");
+    return next(error);
+  }
+
+  return ordersOfThisMonth;
 }
 
 exports.calculateAcquirableAmazonPoint = calculateAcquirableAmazonPoint;
