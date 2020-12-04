e62ed6b2f0652f59faee3810c7f7b18b442b12c8
XX989tmx <issacgravity8@gmail.com>
2020-12-04 09:01:51 +0900

    modified function

--- functions/user-controller-related-functions.js
+++ functions/user-controller-related-functions.js
@@ -1,21 +1,36 @@
 const User = require("../models/user");
 
-async function documentLastLoggedInTime(userId) {
+async function findUserById(userId) {
   let user;
   try {
     user = await User.findById(userId);
   } catch (error) {
     console.log(error);
   }
-  const now = new Date();
-  user.lastLoggedIn.push(now);
-  let message;
+
+  if (!user) {
+    const error = new Error("user data was not found.");
+    return next(error);
+  }
+
+  return user;
+}
+
+async function saveUser(user) {
   try {
     await user.save();
-    message = "logged in time was successfully documented.";
   } catch (error) {
     console.log(error);
   }
+}
 
+async function documentLastLoggedInTime(userId) {
+  let user = await findUserById(userId);
+
+  const now = new Date();
+  user.lastLoggedIn.push(now);
+  await saveUser(user);
+
+  let message = "logged in time was successfully documented.";
   return message;
 }
